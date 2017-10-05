096d99d370ab284ca5d9ad19c76abde9806a8033
Bobby <rwk8144@rit.edu>
2017-10-05 19:13:23 -0400

    Added name to authors

--- WebCheckers/src/main/java/com/webcheckers/model/WebCheckers.java
+++ WebCheckers/src/main/java/com/webcheckers/model/WebCheckers.java
@@ -6,7 +6,7 @@ package main.java.com.webcheckers.model;
  * @author <a href='mailto:ajn3687@g.rit.edu'>AJ Nagashima</a>
  * @author <a href='mailto:gep2494@g.rit.edu'>George-Edward Pinal</a>
  * @author <a href='mailto:kcz3966@g.rit.edu'>Kerri Zalba</a>
- * @author
+ * @author <a href='mailto:rwk8144@g.rit.edu'>Robert Kurdziel</a>
  * @author
  */
 public class WebCheckers {
