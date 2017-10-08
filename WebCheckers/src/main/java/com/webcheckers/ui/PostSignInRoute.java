f2eb999a979bfb5163a3e76aad1851d5024eb7c7
wor3835 <wor3835@rit.edu>
2017-10-08 15:53:39 -0400

    Player and PostGuessRoute classes

---
+++ WebCheckers/src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -0,0 +1,17 @@
+package com.webcheckers.ui;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+/**
+ * The {@code POST /guess} route handler.
+ *
+ * @author <a href='mailto:wor3835@rit.edu'>William Raffaelle</a>
+ */
+public class PostSignInRoute implements Route {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
+}
