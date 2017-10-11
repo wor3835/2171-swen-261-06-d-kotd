ffa5ed8a8f9a48ac3f13806d94e00790b684e3d0
wor3835 <wor3835@rit.edu>
2017-10-11 10:29:40 -0400

    updated PostSignInRoute

--- WebCheckers/src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ WebCheckers/src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -1,12 +1,13 @@
 package com.webcheckers.ui;
 
 import com.webcheckers.appl.PlayerLobby;
-import spark.Request;
-import spark.Response;
-import spark.Route;
-import spark.TemplateEngine;
+import com.webcheckers.model.Player;
+import spark.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.logging.Logger;
 
 /**
@@ -15,13 +16,36 @@ import java.util.logging.Logger;
  * @author <a href='mailto:wor3835@rit.edu'>William Raffaelle</a>
  */
 public class PostSignInRoute implements Route {
+
+    static final String NAME = "myName";
+
     private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
     private final TemplateEngine templateEngine;
     private final PlayerLobby playerLobby;
 
+    /**
+     * Render the WebCheckers SignIn page.
+     *
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     *
+     * @return
+     *   the rendered HTML for the Home page
+     */
     @Override
-    public Object handle(Request request, Response response) throws Exception {
-        return null;
+    public Object handle(Request request, Response response) {
+        //final Session session = request.session();
+        final String name = request.queryParams(NAME);
+
+        Player player = playerLobby.playerSignin(name); // sign in and store player
+
+        LOG.finer("GetHomeRoute is invoked.");
+        //
+        Map<String, Object> vm = new HashMap<>();
+        vm.put("title", "Welcome! " + name);
+        return templateEngine.render(new ModelAndView(vm , "home.ftl"));
     }
 
     public PostSignInRoute(final TemplateEngine templateEngine, PlayerLobby playerLobby) {
