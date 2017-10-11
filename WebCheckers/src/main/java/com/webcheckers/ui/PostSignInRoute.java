2f127eac9aa1b6e67c5c9e985e6900c99d638b8c
AJ Nagashima <ajn3687@g.rit.edu>
2017-10-11 08:35:35 -0400

    Modify classes to accept postsigninroute

--- WebCheckers/src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ WebCheckers/src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -1,8 +1,13 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.appl.PlayerLobby;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.TemplateEngine;
+
+import java.util.Objects;
+import java.util.logging.Logger;
 
 /**
  * The {@code POST /guess} route handler.
@@ -10,8 +15,23 @@ import spark.Route;
  * @author <a href='mailto:wor3835@rit.edu'>William Raffaelle</a>
  */
 public class PostSignInRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
+    private final TemplateEngine templateEngine;
+    private final PlayerLobby playerLobby;
+
     @Override
     public Object handle(Request request, Response response) throws Exception {
         return null;
+    }
+
+    public PostSignInRoute(final TemplateEngine templateEngine, PlayerLobby playerLobby) {
+        // validation
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        //
+        this.templateEngine = templateEngine;
+        //
+        LOG.config("PostSignInRoute is initialized.");
+
+        this.playerLobby = playerLobby;
     }
 }
--- WebCheckers/src/main/java/com/webcheckers/ui/WebServer.java
+++ WebCheckers/src/main/java/com/webcheckers/ui/WebServer.java
@@ -7,6 +7,8 @@ import java.util.logging.Logger;
 
 import com.google.gson.Gson;
 
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Player;
 import spark.TemplateEngine;
 
 
@@ -62,6 +64,7 @@ public class WebServer {
 
   private final TemplateEngine templateEngine;
   private final Gson gson;
+  private final PlayerLobby playerLobby;
 
   //
   // Constructor
@@ -78,13 +81,14 @@ public class WebServer {
    * @throws NullPointerException
    *    If any of the parameters are {@code null}.
    */
-  public WebServer(final TemplateEngine templateEngine, final Gson gson) {
+  public WebServer(final TemplateEngine templateEngine, final Gson gson, final PlayerLobby playerLobby) {
     // validation
     Objects.requireNonNull(templateEngine, "templateEngine must not be null");
     Objects.requireNonNull(gson, "gson must not be null");
     //
     this.templateEngine = templateEngine;
     this.gson = gson;
+    this.playerLobby = playerLobby;
   }
 
   //
@@ -144,7 +148,7 @@ public class WebServer {
     // Shows the Checkers game Sign in page.
     get(SIGN_URL, new GetSignInRoute(templateEngine));
 
-    post(SIGN_URL,  new PostSignInRoute(templateEngine));
+    post(SIGN_URL,  new PostSignInRoute(templateEngine, playerLobby));
 
     //
     LOG.config("WebServer is initialized.");
