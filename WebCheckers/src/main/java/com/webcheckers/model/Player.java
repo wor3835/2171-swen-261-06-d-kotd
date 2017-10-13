6718151cc7127493893ab04254d12015afbb73d9
Bobby <rwk8144@rit.edu>
2017-10-13 18:32:27 -0400

    edited headers

--- WebCheckers/src/main/java/com/webcheckers/model/Player.java
+++ WebCheckers/src/main/java/com/webcheckers/model/Player.java
@@ -5,6 +5,7 @@ package com.webcheckers.model;
  *
  * @author <a href='mailto:wor3835@rit.edu'>William Raffaelle</a>
  * @author <a href='mailto:gep2494@rit.edu'>George-Edward Pinal</a>
+ * @author <a href='mailto:rwk8144@rit.edu'>Robert Kurdziel</a>
  */
 public class Player {
     private String name;
