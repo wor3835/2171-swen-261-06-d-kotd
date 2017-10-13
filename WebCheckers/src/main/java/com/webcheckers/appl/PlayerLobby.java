335cf0a7c6509edb4f2b59b2aa072532a549c760
somthing8wierd <ajn3687@g.rit.edu>
2017-10-13 18:21:38 -0400

    PlayerLobby adds player to PlayerList on signin

--- WebCheckers/src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ WebCheckers/src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -11,7 +11,9 @@ public class PlayerLobby {
     ArrayList<Player> playerList = new ArrayList<Player>();
 
     public Player playerSignin(String name){
-        return new Player(name);
+        Player p = new Player(name);
+        playerList.add(p);
+        return p;
     }
 
     public void add(Player player){
