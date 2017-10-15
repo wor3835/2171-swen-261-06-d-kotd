6b8a05aa743709f031f04090fef189d4f30ad3ea
kzalb <kcz3966@g.rit.edu>
2017-10-15 13:37:51 -0400

    Created Row and Space outline

---
+++ WebCheckers/src/main/java/com/webcheckers/model/Row.java
@@ -0,0 +1,19 @@
+package com.webcheckers.model;
+
+/**
+ * Created by kzalb on 10/15/2017.
+ */
+public class Row {
+
+    private int index;
+
+    public Row(int index)
+    {
+        this.index = index;
+    }
+
+    public int getIndex()
+    {
+        return index;
+    }
+}
---
+++ WebCheckers/src/main/java/com/webcheckers/model/Space.java
@@ -0,0 +1,29 @@
+package com.webcheckers.model;
+
+/**
+ * Created by kzalb on 10/15/2017.
+ */
+public class Space{
+
+    private int cellIdx;
+
+    public Space(int cellIdx)
+    {
+        this.cellIdx = cellIdx;
+    }
+
+    public int getCellIdx()
+    {
+        return cellIdx;
+    }
+
+    public boolean isValid()
+    {
+
+    }
+
+    public Piece getPiece()
+    {
+
+    }
+}
