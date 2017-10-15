3f1183262de2d45d9e959687ced87a719c37ae36
somthing8wierd <ajn3687@g.rit.edu>
2017-10-15 13:51:46 -0400

    Actual implementation of iterator methods

--- WebCheckers/src/main/java/com/webcheckers/model/BoardView.java
+++ WebCheckers/src/main/java/com/webcheckers/model/BoardView.java
@@ -1,7 +1,20 @@
 package com.webcheckers.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by arthu on 10/15/2017.
  */
 public class BoardView {
+    static final int BOARD_LENGTH = 8;
+
+
+    public Iterator<Row> iterator(){
+        List<Row> rows = new ArrayList<>();
+        for(int i = 0; i < BOARD_LENGTH; i++)
+            rows.add(new Row(i));
+        return rows.iterator();
+    }
 }
--- WebCheckers/src/main/java/com/webcheckers/model/Row.java
+++ WebCheckers/src/main/java/com/webcheckers/model/Row.java
@@ -1,5 +1,9 @@
 package com.webcheckers.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by kzalb on 10/15/2017.
  */
@@ -15,5 +19,13 @@ public class Row {
     public int getIndex()
     {
         return index;
+    }
+
+    public Iterator<Space> iterator(){
+        List<Space> spaces = new ArrayList<>();
+        for(int i = 0; i < BoardView.BOARD_LENGTH; i++){
+            spaces.add(new Space(i, (i+index)%2 == 0 ? true : false);
+        }
+        return spaces.iterator();
     }
 }
