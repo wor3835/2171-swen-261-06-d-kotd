5887083f4ff52837ceabc87d115d51136ca09be4
Bobby <rwk8144@rit.edu>
2017-10-25 14:26:46 -0400

    added all enums to master class

--- src/main/java/com/webcheckers/appl/MasterEnum.java
+++ src/main/java/com/webcheckers/appl/MasterEnum.java
@@ -4,5 +4,37 @@ package com.webcheckers.appl;
  * Created by Bobby on 10/25/2017.
  */
 public class MasterEnum {
+    //Message enum
+    public enum messageType {info, error}
+
+    //Piece enums
+    public static enum pieceType{
+        SINGLE,
+        KING
+    }
+
+    public static enum pieceColor{
+        RED,
+        WHITE
+    }
+
+    //GetGameRoute enums
+    enum getGameViewMode {
+        PLAY, SPECTATOR, REPLAY
+    }
+
+    enum getGameActiveColor {
+        RED, WHITE
+    }
+
+    //PostStartRoute enums
+    private enum startViewMode {
+        PLAY, SPECTATOR, REPLAY
+    }
+
+    private enum startActiveColor {
+        RED, WHITE
+    }
+
 
 }
