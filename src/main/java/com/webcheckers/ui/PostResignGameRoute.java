92d9baad96b98bf655de4746a1b4af7142098dc3
wor3835 <wor3835@rit.edu>
2017-10-26 21:43:36 -0400

    postresign skele

---
+++ src/main/java/com/webcheckers/ui/PostResignGameRoute.java
@@ -0,0 +1,22 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.model.Player;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+/**
+ * Created by wor3835 on 10/26/2017.
+ */
+public class PostResignGameRoute implements Route {
+    private final Player player; // player that is resigning
+
+    public PostResignGameRoute(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
+}
