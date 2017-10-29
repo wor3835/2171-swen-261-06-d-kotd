61741de1eb4a2634d4858769a379395d930a5735
somthing8wierd <ajn3687@g.rit.edu>
2017-10-28 20:07:50 -0400

    Implement the following
    PostSubmitturn: applying a move
    Board: Making a move

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -68,8 +68,5 @@ public class Space{
         return m1;
     }
 
-    public void setPiece(Piece p) {
-        piece = p;
-    }
 }
 
--- src/main/java/com/webcheckers/ui/PostSubmitTurn.java
+++ src/main/java/com/webcheckers/ui/PostSubmitTurn.java
@@ -1,40 +1,36 @@
 package com.webcheckers.ui;
 
+import com.google.gson.Gson;
+import com.webcheckers.appl.MasterEnum;
 import com.webcheckers.model.*;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.Session;
 
 /**
  * Created by wor3835 on 10/25/2017.
  */
 public class PostSubmitTurn implements Route {
-    private final Move move;
-    private final Game game;
-    private final Piece piece;
-
-    public PostSubmitTurn(Move move, Game game, Piece piece) {
-        this.move = move;
-        this.game = game;
-        this.piece = piece;
-    }
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
+        String body = request.body();
+        Gson g = new Gson();
+        Move move = g.fromJson(body, Move.class);
+
+        Session session = request.session();
+        Game game = session.attribute(GetGameRoute.GAME_ATTR);
         if(game.movesList.contains(move)) {
-            int startRow = move.getStart().getRow();
-            int startCol = move.getEnd().getCol();
-            int endRow = move.getEnd().getRow();
-            int endCol = move.getEnd().getCol();
             Board b1 = game.getB1();
             Board b2 = game.getB2();
-            b1.makeMove(endRow, endCol, piece);
-            b2.makeMove(endRow, endCol, piece);
-            Piece p1 = b1.getPieceAt(startRow, startCol);
-            Piece p2 = b2.getPieceAt(startRow, startCol);
-            p1 = null;
-            p2 = null;
-            return null;
+            if(session.attribute(GetGameRoute.ACTIVE_COLOR) == MasterEnum.Color.RED) {
+                b1.makeMove(move);
+                b2.inverseMove(move);
+            } else {
+                b1.inverseMove(move);
+                b2.makeMove(move);
+            }
         }
         return null;
     }
