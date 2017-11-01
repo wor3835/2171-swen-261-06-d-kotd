70f20ced30d14a6fff8fcdb13091703cae3be938
somthing8wierd <ajn3687@g.rit.edu>
2017-11-01 10:33:12 -0400

    Clean valid moves and force captures on a piece

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -9,7 +9,7 @@ import java.util.List;
  * Created by kzalb on 10/15/2017.
  * Represents a space on the board
  */
-public class Space{
+public class Space {
 
     private int cellIdx;
     private boolean validSpace;
@@ -18,12 +18,12 @@ public class Space{
 
     /**
      * Physical space on the game board
+     *
      * @param cellIdx id of cell
-     * @param valid if space is on board or not
-     * @param piece game piece on space
+     * @param valid   if space is on board or not
+     * @param piece   game piece on space
      */
-    public Space(int cellIdx, boolean valid, Piece piece)
-    {
+    public Space(int cellIdx, boolean valid, Piece piece) {
         this.cellIdx = cellIdx;
         this.validSpace = valid;
         this.piece = piece;
@@ -32,88 +32,92 @@ public class Space{
     /**
      * @return id of cell
      */
-    public int getCellIdx()
-    {
+    public int getCellIdx() {
         return cellIdx;
     }
 
     /**
      * @return if space is valid
      */
-    public boolean isValid()
-    {
+    public boolean isValid() {
         return validSpace;
     }
 
     /**
      * @return game piece on space
      */
-    public Piece getPiece()
-    {
+    public Piece getPiece() {
         return piece;
     }
 
-    public void kingMe(){
+    public void kingMe() {
         piece = new King(piece);
     }
 
     public ArrayList<Move> validMoves(Board b, int row, int col) {
         ArrayList<Move> m1 = new ArrayList<>();
-        if (b.hasPiece(row, col)) {
-            Piece curr = b.getPieceAt(row, col);
-            if (curr.getType().equals(MasterEnum.PieceType.SINGLE)) {
-                if(row - 1 >= 0 && col + 1 < BoardView.BOARD_LENGTH) {
-                    if(!(b.hasPiece(row - 1, col + 1))) {
-                        m1.add(new Move(new Position(row, col), new Position(row - 1, col + 1)));
-                    }
-                    else if(row-2 >= 0 && col+2 < BoardView.BOARD_LENGTH && !(b.hasPiece(row-2, col+2))){
-                        m1.add(new Move(new Position(row,col), new Position(row-2, col+2)));
-                    }
+
+        if (row - 1 >= 0 && col + 1 < BoardView.BOARD_LENGTH) {
+            if (!(b.hasPiece(row - 1, col + 1)))
+                m1.add(new Move(new Position(row, col), new Position(row - 1, col + 1)));
+        }
+        if (row - 1 >= 0 && col - 1 >= 0) {
+            if (!(b.hasPiece(row - 1, col - 1)))
+                m1.add(new Move(new Position(row, col), new Position(row - 1, col - 1)));
+        }
+        if (piece.getType().equals(MasterEnum.PieceType.KING)) {
+            if (row + 1 < BoardView.BOARD_LENGTH && col + 1 < BoardView.BOARD_LENGTH) {
+                if (!(b.hasPiece(row + 1, col + 1)))
+                    m1.add(new Move(new Position(row, col), new Position(row + 1, col + 1)));
+            }
+            if (row + 1 < BoardView.BOARD_LENGTH && col - 1 >= 0) {
+                if (!(b.hasPiece(row + 1, col - 1)))
+                    m1.add(new Move(new Position(row, col), new Position(row + 1, col - 1)));
+            }
+        }
+        return validJumps(b, row, col, m1);
+    }
+
+    public ArrayList<Move> validJumps(Board b, int row, int col, ArrayList<Move> moves){
+        boolean noJumps = true;
+
+        if (row - 1 >= 0 && col + 1 < BoardView.BOARD_LENGTH) {
+            if (b.hasPiece(row - 1, col + 1) &&
+                    (row - 2 >= 0 && col + 2 < BoardView.BOARD_LENGTH && !(b.hasPiece(row - 2, col + 2)))) {
+                if(noJumps) {noJumps = false; moves.clear();}
+
+                moves.add(new Move(new Position(row, col), new Position(row - 2, col + 2)));
+            }
+        }
+        if (row - 1 >= 0 && col - 1 >= 0) {
+            if (b.hasPiece(row - 1, col - 1) &&  (row - 2 >= 0 && col - 2 >= 0 && !(b.hasPiece(row - 2, col - 2)))) {
+                if(noJumps) {noJumps = false; moves.clear();}
+
+                moves.add(new Move(new Position(row, col), new Position(row - 2, col - 2)));
+            }
+        }
+        if (piece.getType().equals(MasterEnum.PieceType.KING)) {
+            if (row + 1 < BoardView.BOARD_LENGTH && col + 1 < BoardView.BOARD_LENGTH) {
+                if (b.hasPiece(row + 1, col + 1) &&
+                        (row + 2 < BoardView.BOARD_LENGTH && col + 2 < BoardView.BOARD_LENGTH && !(b.hasPiece(row + 2, col + 2)))) {
+                    if(noJumps) {noJumps = false; moves.clear();}
+
+                    moves.add(new Move(new Position(row, col), new Position(row + 2, col + 2)));
                 }
-                if(row - 1 >= 0 && col - 1 >= 0) {
-                    if(!(b.hasPiece(row - 1, col - 1))) {
-                        m1.add(new Move(new Position(row, col), new Position(row -1, col - 1)));
-                    }
-                    else if(row-2 >= 0 && col-2 >= 0 && !(b.hasPiece(row-2, col-2))){
-                        m1.add(new Move(new Position(row,col), new Position(row-2, col-2)));
-                    }
-                }
-            } else if (curr.getType().equals(MasterEnum.PieceType.KING)) {
-                if(row - 1 >= 0 && col + 1 < BoardView.BOARD_LENGTH) {
-                    if(!(b.hasPiece(row - 1, col + 1))) {
-                        m1.add(new Move(new Position(row, col), new Position(row - 1, col + 1)));
-                    }
-                    else if(row-2 >= 0 && col+2 < BoardView.BOARD_LENGTH && !(b.hasPiece(row-2, col+2))) {
-                        m1.add(new Move(new Position(row, col), new Position(row - 2, col + 2)));
-                    }
-                }
-                if(row - 1 >= 0 && col - 1 >= 0) {
-                    if(!(b.hasPiece(row - 1, col - 1))) {
-                        m1.add(new Move(new Position(row, col), new Position(row -1, col - 1)));
-                    }
-                    else if(row-2 >= 0 && col-2 >= 0 && !(b.hasPiece(row -2, col -2))) {
-                        m1.add(new Move(new Position(row, col), new Position(row - 2, col - 2)));
-                    }
-                }
-                if(row + 1 < BoardView.BOARD_LENGTH && col + 1 < BoardView.BOARD_LENGTH) {
-                    if(!(b.hasPiece(row + 1, col + 1))) {
-                        m1.add(new Move(new Position(row, col), new Position(row + 1, col + 1)));
-                    }
-                    else if(row+2 < BoardView.BOARD_LENGTH && col+2 < BoardView.BOARD_LENGTH && !(b.hasPiece(row+2, col+2))) {
-                        m1.add(new Move(new Position(row, col), new Position(row + 2, col + 2)));
-                    }
-                }
-                if(row + 1 < BoardView.BOARD_LENGTH && col - 1 >= 0) {
-                    if(!(b.hasPiece(row + 1, col - 1))) {
-                        m1.add(new Move(new Position(row, col), new Position(row + 1, col - 1)));
-                    }
-                    else if(row+2 < BoardView.BOARD_LENGTH && col-2 >= 0 && !(b.hasPiece(row+2, col-2))) {
-                        m1.add(new Move(new Position(row, col), new Position(row + 2, col - 2)));
-                    }
+            }
+            if (row + 1 < BoardView.BOARD_LENGTH && col - 1 >= 0) {
+                if (b.hasPiece(row + 1, col - 1) &&
+                        (row + 2 < BoardView.BOARD_LENGTH && col - 2 >= 0 && !(b.hasPiece(row + 2, col - 2)))) {
+                    if(noJumps) {noJumps = false; moves.clear();}
+
+                    moves.add(new Move(new Position(row, col), new Position(row + 2, col - 2)));
                 }
             }
         }
-        return m1;
+
+        //if(noJumps)
+            return moves;
+        //return validJumps(b, row, col, moves);
     }
 
 }
